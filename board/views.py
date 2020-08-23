510ab876dca50382aeed605d6589f363596437c1
jung yura <jungyr24@hanmail.net>
2020-08-24 05:52:40 +0900

    문제 번호 입력하면 문제 페이지로 이동

--- board/views.py
+++ board/views.py
@@ -1,31 +1,76 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .forms import ProblemInputForm
-from board.scraping import problem_scraping
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+import requests
+import math
+from bs4 import BeautifulSoup
+
 
 def main_page(request):
-    if( request.user.is_authenticated == True):
+    if request.user.is_authenticated == True:
         return render(request, 'board/base.html', {})
     else:
         return redirect('login')
-
-# 문제 긁어오기
-def prob_page(request):
-    return render(request, 'board/test.html')
-
 
 
 def tab_page(request):
     return render(request, 'board/tab.html', {})
 
 
-def new_number(request):
-    if request.method == 'POST':
-        form = ProblemInputForm(request.POST)
-        if form.is_valid():
-            number=form.cleaned_data['number']
-
-            return redirect('prob_page', number)
+def get_prob_number(request):
+    if 'q' in request.GET:
+        number = request.GET['q']
     else:
-        form = ProblemInputForm()
+        message = 'plz enter the problem number'
+        return HttpResponse(message)
+    url = 'http://boj.kr/' + str(number)
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    problem = {}
+    similar_problem = []
+    info_key = []
+    info_value = []
+    problem['문제번호'] = number
+    for tag in soup.select('.col-md-12'):
+        for title in tag.select('#problem_title'):
+            problem['문제제목'] = title.text
 
-    return render(request, 'board/base.html')
+        for prob in tag.select('#problem_description p'):
+            problem['문제'] = prob.text
+
+        for in_put in tag.select('#input p'):
+            problem['입력'] = in_put.text
+
+        for output in tag.select('#output p'):
+            problem['출력'] = output.text
+
+        for key in soup.select('.col-md-12 .table th'):
+            info_key.append(key.text.replace(' ',''))
+
+        for value in soup.select('.col-md-12 .table td'):
+            info_value.append(value.text.replace(' ', ''))
+
+        for similar_prob in tag.select('#problem_association li'):
+            similar_problem.append(similar_prob.text)
+
+    for key, value in zip(info_key, info_value):
+        problem[key] = value
+
+    input_list = []
+    output_list = []
+
+    for idx, ex in enumerate(soup.select('.col-md-12 .col-md-6 .sampledata')):
+        cnt = math.floor(idx / 2) + 1
+
+        if idx % 2 == 0:
+            input_list.append(ex.text)
+        else:
+            output_list.append(ex.text)
+
+    problem['입력예시'] = input_list
+    problem['출력예시'] = output_list
+
+    if len(similar_problem) != 0:
+        problem['비슷한문제'] = similar_problem
+
+
+    return render(request, 'board/test.html', {'problem': problem})
